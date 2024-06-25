45fc878ffedd888170f709f8f6cff421a5b3c34f
itachi-uchiha12 <163325793+itachi-uchiha12@users.noreply.github.com>
2024-06-25 12:48:09 +0530

    Add files via upload

---
+++ size.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+public class size {
+
+	public static void main(String[] args) 
+	{
+		LinkedList<String> names = new LinkedList();
+        names.add("Batman");
+        names.add("Captain America");
+        names.add("Blue Beetle");
+        names.add("Superman");
+        names.add("Thor");
+        System.out.println(names.size());
+	}
+
+}
+
+/*output
+5    */
