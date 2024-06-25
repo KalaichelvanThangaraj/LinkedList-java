10b949bd36bdfb34af7e5b630a18234c2fb98128
itachi-uchiha12 <163325793+itachi-uchiha12@users.noreply.github.com>
2024-06-25 12:44:22 +0530

    Add files via upload

---
+++ getFirst.java
@@ -0,0 +1,23 @@
+
+
+import java.util.LinkedList;
+
+public class getFirst{
+
+	public static void main(String[] args) {
+		LinkedList<String> names = new LinkedList();
+        names.add("itachi uchiha");
+        names.add("obito uchiha");
+        names.add("madara uchiha");
+        names.add("Sisui uchiha");
+        names.add("minato namekaze");
+        System.out.print(names.getFirst());
+
+	}
+
+}
+
+
+ /*  output
+  itachi uchiha */
+
